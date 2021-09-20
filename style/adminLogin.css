055903d7a4fdbbd466a222e8596b8d2291e69750
Sugam Kumar Singh <72367458+SugamKumarSingh@users.noreply.github.com>
2021-09-20 23:56:55 +0530

    Create adminLogin.css

---
+++ style/adminLogin.css
@@ -0,0 +1,42 @@
+form {
+    position: relative;
+    top: 30vh;
+    margin: auto;
+    padding: 30px 60px;
+    border-radius: 1.6rem;
+    background: linear-gradient(20deg, tomato, orange);
+}
+.top-heading {
+    text-align: center;
+    font-size: 1.6rem;
+    font-weight: 900;
+}
+form input[type="text"],
+form input[type="password"] {
+    width: 100%;
+    font-weight: bold;
+    font-size: 1.3rem;
+    margin: 10px;
+    padding: 6px;
+    border-radius: 8px;
+    border: none;
+}
+#adminBtn {
+    margin: 8px;
+    margin-top: 12px;
+    float: right;
+    color: white;
+    font-size: 1.3rem;
+    font-weight: bold;
+    padding: 2px 8px;
+    border-radius: 8px;
+    border: none;
+    background: linear-gradient(20deg, rgb(7, 73, 255), rgb(2, 225, 255));
+}
+#adminBtn:hover {
+    background: linear-gradient(200deg, rgb(7, 73, 255), rgb(2, 225, 255));
+}
+.foot {
+    position: fixed;
+    bottom: 0;
+}
